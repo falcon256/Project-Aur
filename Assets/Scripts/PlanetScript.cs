0d9bb57a27628fde7074f8d9caacbab625894909
falcon256 <256games@gmail.com>
2019-02-06 14:39:54 -0700

    More planet stuff

--- Assets/Scripts/PlanetScript.cs
+++ Assets/Scripts/PlanetScript.cs
@@ -10,31 +10,47 @@ public class PlanetScript : MonoBehaviour
     public MeshRenderer cloudMeshRenderer = null;
     public Material surfaceMaterial = null;
     public Material cloudMaterial = null;
+    public float planetSize = 1.0f; // 1.0 = 1 earth;
+    public float planetMass = 1.0f; // 1.0 = 1 earth;
     private Texture2D surfaceDiffuse = null;
     private Texture2D surfaceEmission = null;
     private Texture2D surfaceDisplace = null;
+    private Texture2D surfaceDetail = null;
     private Texture2D surfaceSpecular = null;
+    private int textureWidth = 0;
+    private int textureHeight = 0;
+    private int generationSeed = 0;
+    
     
 
     void Start()
     {
-
-        surfaceDiffuse = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-        surfaceEmission = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-        surfaceDisplace = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-        surfaceSpecular = new Texture2D(256, 256, TextureFormat.RGBA32, false);
-        initializeRGBA32Texture(surfaceDisplace);
+        textureWidth = Mathf.NextPowerOfTwo((int)planetSize * 256);
+        textureHeight = Mathf.NextPowerOfTwo((int)((planetSize * 256) / 1.5f));
+        surfaceDiffuse = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+        surfaceEmission = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+        surfaceDisplace = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+        surfaceDetail = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+        surfaceSpecular = new Texture2D(textureWidth, textureHeight, TextureFormat.RGBA32, false);
+        initializeRGBA32HeightTexture(surfaceDisplace);
+        initializeRGBA32HeightTexture(surfaceDetail);
         //for(int i = 0; i < surfaceMaterial.GetTexturePropertyNames().Length; i++)
-       //     Debug.Log(surfaceMaterial.GetTexturePropertyNames()[i]);
+        //     Debug.Log(surfaceMaterial.GetTexturePropertyNames()[i]);
         surfaceMaterial.SetTexture("_ParallaxMap", surfaceDisplace);
-        
+        surfaceMaterial.SetTexture("_BumpMap", surfaceDetail);
 
-        
+
     }
     
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.Rotate(0, 0.1f, 0);
+
     }
 
     private void initializeR16Texture(Texture2D tex)
@@ -58,7 +74,9 @@ public class PlanetScript : MonoBehaviour
         }
        
     }
-    private void initializeRGBA32Texture(Texture2D tex)
+
+
+    private void initializeRGBA32HeightTexture(Texture2D tex)
     {
         if (tex == null)
         {
@@ -66,29 +84,20 @@ public class PlanetScript : MonoBehaviour
             return;
         }
         var data = tex.GetRawTextureData<Color32>();
-        byte[] bits = new byte[tex.width * tex.height * 4];
+        float equatorMultiplier = 0;
         for (int x = 0; x < tex.width; x++)
         {
             for (int y = 0; y < tex.height; y++)
             {
-                byte n = (byte)(Perlin.OctavePerlin(x / 0.1f, y / 0.1f, 0.5, 8, 0.1) * 255);
-                //tex.SetPixel(x, y, new Color(n, 0, 0, n));
+                equatorMultiplier = Mathf.Min(0 + y + generationSeed, tex.height - y + generationSeed) /((float)(tex.height));
+                byte n = (byte)(Perlin.OctavePerlin(x / ((float)tex.width/16.0f), y / ((float)tex.height/16.0f), 0.5,10, 0.5) * 255 * equatorMultiplier);
                 Color32 nn = new Color32(n, n, n, 255);
                 data[x + y * tex.width] = nn;
-                //bits[(x * 4) + (y * 4 * tex.width)] =   (byte) nibble;
-                //bits[(x * 4) + (y * 4 * tex.width)+1] = (byte)(nibble << 8);
-                //bits[(x * 4) + (y * 4 * tex.width)+2] = (byte)(nibble << 16);
-                //bits[(x * 4) + (y * 4 * tex.width)+3] = (byte)(nibble << 24);
-                //bits[(x * 4) + (y * 4 * tex.width)] = 0;
-                //bits[(x * 4) + (y * 4 * tex.width)+1] = 0;
-                //bits[(x * 4) + (y * 4 * tex.width)+2] = 0;
-                //bits[(x * 4) + (y * 4 * tex.width)+3] = 0;
-                // Debug.Log(nibble);
             }
 
         }
-        Debug.Log("Fuckle");
-        //tex.LoadRawTextureData(bits);
+        tex.filterMode = FilterMode.Point;
+        tex.wrapMode = TextureWrapMode.Repeat;
         tex.Apply();
     }
 }
